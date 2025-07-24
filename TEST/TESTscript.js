2a38b5b672972fd77c80d0f4c1e4581da4e8adee
Sonia <soniap.lamagicienne@hotmail.com>
2025-07-24 13:37:47 +0200

    Test de modal pour galerie, ajout de page de test spécifiques html css et js

---
+++ TEST/TESTscript.js
@@ -0,0 +1,27 @@
+// MODAL 
+
+//get the modal
+let modal = document.getElementById("myModal"); 
+
+// get the button that opens the modal
+let btn = document.getElementById("myBtn"); 
+
+// get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// when the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// when the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none"; 
+}
+
+//when the user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none"; 
+    }
+}
